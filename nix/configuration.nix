dc1be43540f1334cb39d79113ac7f8d5f0dc1529
Nathan Gasser <hello@gougoule.ch>
2026-01-26 21:44:24 +0100

    a few tweaks and tab spaces

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -1,120 +1,121 @@
 { config, pkgs, ... }:
 
 let
-  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-25.11.tar.gz";
+    home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-25.11.tar.gz";
 in
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-      ./temp.nix
-      (import "${home-manager}/nixos")
-    ];
-
-  nix.nixPath = [
-    "nixos-config=/home/nathangasser/git/monorepo/nix/configuration.nix"  # Your desired default path
-    "nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos"
-  ];
-
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  networking.hostName = "nixos"; # Define your hostname.
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  time.timeZone = "Europe/Zurich";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.displayManager.gdm.enable = true;
-  services.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "ch";
-    variant = "fr";
-  };
-
-  # Configure console keymap
-  console.keyMap = "fr_CH";
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-  };
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.nathangasser = {
-    isNormalUser = true;
-    description = "Nathan Gasser";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-    ];
-  };
-
-  home-manager.users.nathangasser = { pkgs, ... }: {
-    home.username = "nathangasser";
-    home.homeDirectory = "/home/nathangasser";
-    home.stateVersion = "25.11";
-
-    programs.git = {
-      enable = true;
-      settings = {
-        user.name = "Nathan Gasser";
-        user.email = "hello@gougoule.ch";
-      };
-    };
-
-    wayland.windowManager.hyprland = {
-      enable = true;
-      settings = {
-        "$mod" = "SUPER";
-        "$terminal" = "ghostty";
-        "$browser" = "xdg-open https://";
-        bind = [
-          "$mod, Return, exec, $terminal"
+    imports =
+        [ # Include the results of the hardware scan.
+        ./hardware-configuration.nix
+            ./temp.nix
+            (import "${home-manager}/nixos")
         ];
-        input = {
-          kb_layout = "ch";
-          kb_variant = "fr";
-        };
-      };
+
+    nix.nixPath = [
+        "nixos-config=/home/nathangasser/git/monorepo/nix/configuration.nix"  # Your desired default path
+            "nixpkgs=/nix/var/nix/profiles/per-user/root/channels/nixos"
+    ];
+
+    # Bootloader.
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+
+    networking.hostName = "nixos"; # Define your hostname.
+
+    # Enable networking
+        networking.networkmanager.enable = true;
+
+    time.timeZone = "Europe/Zurich";
+
+    # Select internationalisation properties.
+    i18n.defaultLocale = "en_US.UTF-8";
+
+    # Enable the X11 windowing system.
+    services.xserver.enable = true;
+
+    # Enable the GNOME Desktop Environment.
+    services.displayManager.gdm.enable = true;
+    services.desktopManager.gnome.enable = true;
+
+    # Configure keymap in X11
+    services.xserver.xkb = {
+        layout = "ch";
+        variant = "fr";
     };
-  };
+
+    # Configure console keymap
+    console.keyMap = "fr_CH";
+
+    # Enable CUPS to print documents.
+    services.printing.enable = true;
+
+    # Enable sound with pipewire.
+    services.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+    };
+
+    users.users.nathangasser = {
+        isNormalUser = true;
+        description = "Nathan Gasser";
+        extraGroups = [ "networkmanager" "wheel" ];
+        packages = with pkgs; [
+        ];
+    };
+
+    home-manager.users.nathangasser = { pkgs, ... }: {
+        home.username = "nathangasser";
+        home.homeDirectory = "/home/nathangasser";
+        home.stateVersion = "25.11";
+
+        programs.git = {
+            enable = true;
+            settings = {
+                user.name = "Nathan Gasser";
+                user.email = "hello@gougoule.ch";
+            };
+        };
+
+        wayland.windowManager.hyprland = {
+            enable = true;
+            settings = {
+                "$mod" = "SUPER";
+                "$terminal" = "ghostty";
+                "$browser" = "xdg-open https://";
+                bind = [
+                    "$mod, Return, exec, $terminal"
+                        "$mod, d, exec, wofi --show drun"
+                ];
+                input = {
+                    kb_layout = "ch";
+                    kb_variant = "fr";
+                };
+            };
+        };
+    };
 
 
-  programs.firefox.enable = true;
-  programs.hyprland.enable = true;
+    programs.firefox.enable = true;
+    programs.hyprland.enable = true;
 
-  nixpkgs.config.allowUnfree = true;
+    nixpkgs.config.allowUnfree = true;
 
-  environment.systemPackages = with pkgs; [
-    vim
-    git
-    brave
-    gh
-    ghostty
-    fastfetch
-    pavucontrol
-  ];
+    environment.systemPackages = with pkgs; [
+        vim
+            git
+            brave
+            gh
+            ghostty
+            fastfetch
+            pavucontrol
+            wofi
+    ];
 
-  system.stateVersion = "25.11"; # Did you read the comment?
+    system.stateVersion = "25.11"; # Did you read the comment?
 
 }
--- nix/desktop.nix
+++ nix/desktop.nix
@@ -1,21 +1,21 @@
 { config, pkgs, lib, ... }:
 
 {
-  # NVIDIA proprietary drivers (NixOS 25.11+ changes)
-  hardware.nvidia = {
-    modesetting.enable = true;
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-    open = false;  # Required for proprietary drivers >= 560
-  };
+    # NVIDIA proprietary drivers (NixOS 25.11+ changes)
+    hardware.nvidia = {
+        modesetting.enable = true;
+        nvidiaSettings = true;
+        package = config.boot.kernelPackages.nvidiaPackages.stable;
+        open = false;  # Required for proprietary drivers >= 560
+    };
 
-  # X server configuration for NVIDIA
-  services.xserver.videoDrivers = [ "nvidia" ];
+    # X server configuration for NVIDIA
+    services.xserver.videoDrivers = [ "nvidia" ];
 
-  # OpenGL support (driSupport removed in newer NixOS)
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
+    # OpenGL support (driSupport removed in newer NixOS)
+    hardware.graphics = {
+        enable = true;
+        enable32Bit = true;
+    };
 }
 
