b1e9096101db5a358cf49a2b26a4aad8fe28f522
Nathan Gasser <hello@gougoule.ch>
2026-02-07 17:32:02 +0100

    idk

--- server-software/src/lib/auth.svelte.ts
+++ server-software/src/lib/auth.svelte.ts
@@ -1,12 +1,10 @@
-export const auth: {
-	username: string | null;
-	password: string | null;
-	hashedPassword: string | null;
-} = $state({
-	username: null,
-	password: null,
-	hashedPassword: null
-});
+class AuthState {
+	username: string | null = $state(null);
+	password: string | null = $state(null);
+	hashedPassword: string | null = $state(null);
+}
+
+export const auth = new AuthState();
 
 export function setAuth(username: string, password: string, hashedPassword: string) {
 	auth.username = username;
