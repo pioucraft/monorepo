d33358c5fd43145379da1e91733ddd495a3590e4
Nathan Gasser <hello@gougoule.ch>
2026-02-07 19:16:35 +0100

    fix node modules

--- server-software/default.nix
+++ server-software/default.nix
@@ -3,7 +3,32 @@
 let
   bun = pkgs.bun;
   name = "server-software";
-  src = ./.;
+  src = builtins.path {
+    path = ./.;
+    name = "server-software-src";
+    filter = path: type:
+      let base = builtins.baseNameOf path; in
+      base != ".svelte-kit"
+      && base != "build"
+      && base != "result"
+      && base != ".env"
+      && base != "node_modules";
+  };
+  node_modules = pkgs.stdenv.mkDerivation {
+    name = "server-software-node_modules";
+    inherit src;
+    nativeBuildInputs = [ bun ];
+    buildPhase = ''
+      export HOME=$(mktemp -d)
+      bun install --frozen-lockfile --ignore-scripts
+    '';
+    installPhase = ''
+      cp -r node_modules $out
+    '';
+    outputHashMode = "recursive";
+    outputHashAlgo = "sha256";
+    outputHash = "";
+  };
 in
 pkgs.stdenv.mkDerivation {
   inherit name src;
@@ -12,7 +37,8 @@ pkgs.stdenv.mkDerivation {
 
   buildPhase = ''
     export HOME=$(mktemp -d)
-    bun install --frozen-lockfile --ignore-scripts
+    cp -r ${node_modules} node_modules
+    chmod -R u+w node_modules
     bun node_modules/.bin/svelte-kit sync
     bun node_modules/.bin/vite build
   '';
