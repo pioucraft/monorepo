aec0119a4af5ae08cd8b585fe2a62b95390d5a66
Nathan Gasser <hello@gougoule.ch>
2026-02-14 21:24:11 +0100

    script

--- nix-server/download-music.sh
+++ nix-server/download-music.sh
@@ -8,21 +8,10 @@ fi
 
 URL="$1"
 MUSIC_DIR="/home/nix/git/monorepo/data/music"
-NIX_GROUP="$(id -gn nix 2>/dev/null || true)"
+mkdir -p $MUSIC_DIR && sudo chown -R nix:nix $MUSIC_DIR
 
-if [ -n "$NIX_GROUP" ]; then
-    if ! install -d -m 0755 -o nix -g "$NIX_GROUP" "$MUSIC_DIR"; then
-        echo "Failed to create $MUSIC_DIR with nix ownership"
-        exit 1
-    fi
-else
-    if ! install -d -m 0755 -o nix "$MUSIC_DIR"; then
-        echo "Failed to create $MUSIC_DIR with nix ownership"
-        exit 1
-    fi
-fi
 
-machinectl shell wireguard /bin/sh -c "yt-dlp -x --audio-format mp3 --audio-quality 0 \
+sudo nixos-container run wireguard -- /bin/sh -c "yt-dlp -x --audio-format mp3 --audio-quality 0 \
     --embed-metadata --embed-thumbnail \
     --parse-metadata 'playlist_index:%(track_number)s' \
     --parse-metadata 'release_year:%(meta_date)s' \
@@ -30,3 +19,5 @@ machinectl shell wireguard /bin/sh -c "yt-dlp -x --audio-format mp3 --audio-qual
     --ppa \"EmbedThumbnail+ffmpeg_o:-c:v mjpeg -vf \\\"crop='if(gt(ih,iw),iw,ih)':'if(gt(iw,ih),ih,iw)'\\\"\" \
     -o '$MUSIC_DIR/%(artist)s %(album)s %(playlist_index)s - %(title)s.%(ext)s' \
     '$URL'"
+
+sudo chown -R nix:nix $MUSIC_DIR
