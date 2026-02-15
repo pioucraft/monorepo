616fa5ab011376930194c78ac621f0f8194b670c
Nathan Gasser <hello@gougoule.ch>
2026-02-15 13:39:50 +0100

    is gonna work now ?

--- nix-server/download-music.sh
+++ nix-server/download-music.sh
@@ -8,10 +8,10 @@ fi
 
 URL="$1"
 MUSIC_DIR="/home/nix/git/monorepo/data/music"
-mkdir -p $MUSIC_DIR && sudo chown -R nix $MUSIC_DIR
+mkdir -p $MUSIC_DIR && chown -R nix $MUSIC_DIR
 
 
-sudo nixos-container run wireguard -- /bin/sh -c "yt-dlp -x --audio-format mp3 --audio-quality 0 \
+nixos-container run wireguard -- /bin/sh -c "yt-dlp -x --audio-format mp3 --audio-quality 0 \
     --embed-metadata --embed-thumbnail \
     --parse-metadata 'playlist_index:%(track_number)s' \
     --convert-thumbnails jpg \
@@ -20,4 +20,4 @@ sudo nixos-container run wireguard -- /bin/sh -c "yt-dlp -x --audio-format mp3 -
     -o '$MUSIC_DIR/%(artist)s %(album)s %(playlist_index)s - %(title)s.%(ext)s' \
     '$URL'"
 
-sudo chown -R nix $MUSIC_DIR
+chown -R nix $MUSIC_DIR
