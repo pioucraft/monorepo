9eca4cae2b1dc09b8031c91b4e0bc9e06c7e3d54
Nathan Gasser <hello@gougoule.ch>
2026-02-15 14:16:45 +0100

    fuck THIS SHIT

--- nix-server/configuration.nix
+++ nix-server/configuration.nix
@@ -168,8 +168,7 @@ in
             Restart = "always";
             User = "nix";
             Environment = "PATH=/run/current-system/sw/bin:/bin:/usr/bin:/usr/local/bin";
-            NoNewPrivileges = lib.mkForce false;
-            RestrictSUIDSGID = lib.mkForce false;
+
         };
     };
 
