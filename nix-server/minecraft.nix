26eed0a89e3b604763c43ff27583969c81e17c92
Nathan Gasser <hello@gougoule.ch>
2026-02-18 10:18:23 +0100

    add minecraft

---
+++ nix-server/minecraft.nix
@@ -0,0 +1,31 @@
+{ config, pkgs, ... }:
+
+{
+    services.minecraft-server = {
+        enable = true;
+        eula = true;
+
+        # Store world + configs here
+        dataDir = "/home/nix/data/minecraft";
+
+        # Make whitelist/ops/etc managed declaratively by NixOS
+        declarative = true;
+
+        # Restrict to whitelist
+        serverProperties = {
+            white-list = true;
+            enforce-whitelist = true;
+            # server-port = 25565; # optional
+        };
+
+        # Whitelist is a mapping: username -> UUID
+        whitelist = {
+            # Replace with einPfannkuchen's real UUID:
+            einPfannkuchen = "282d2416-c08d-42aa-84ee-f16e0c230cc5";
+        };
+
+        # Optional: open firewall for the port
+        openFirewall = true;
+    };
+}
+
